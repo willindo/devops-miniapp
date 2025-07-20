69d38ee2941705d964142b3fadc326b676c6aff9
willindo <badshanoordeen@gmail.com>
2025-07-20 10:01:50 +0530

    your

--- app/index.js
+++ app/index.js
@@ -7,7 +7,7 @@ fastify.get('/health', async () => {
 const start = async () => {
   try {
     await fastify.listen({ port: 3000, host: '0.0.0.0' });
-    console.log('Server running on http://localhost:3000');
+    console.log('your Server running on http://localhost:3000');
   } catch (err) {
     fastify.log.error(err);
     process.exit(1);
